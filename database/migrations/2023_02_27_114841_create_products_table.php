93944033596cf7cf342c32da071948a655b6af55
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-02-27 11:59:17 +0800

    added products model and db

---
+++ database/migrations/2023_02_27_114841_create_products_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->float('price')->nullable()->default(0.00);
+            $table->string('type')->nullable()->default('Application'); // possible value "Service", "Application", and "Device"
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductsSeeder extends Seeder
+{
+    public function run(): void
+    {
+
+    }
+}
