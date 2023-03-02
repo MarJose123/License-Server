063f010dcdd6c283a6f5ccf33dd4783c5c77fdb0
Marjose <hypernate1@gmail.com>
2023-03-02 16:31:15 +0800

    added logic for generating key upon creating license record

--- app/Filament/Resources/LicensesResource/Pages/CreateLicenses.php
+++ app/Filament/Resources/LicensesResource/Pages/CreateLicenses.php
@@ -3,10 +3,26 @@
 namespace App\Filament\Resources\LicensesResource\Pages;
 
 use App\Filament\Resources\LicensesResource;
+use App\Helpers\LicenseKeyGenerator;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateLicenses extends CreateRecord
 {
     protected static string $resource = LicensesResource::class;
+
+    /**
+     * @throws \Exception
+     */
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['license_key'] = (new LicenseKeyGenerator())->generateUnique();
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
